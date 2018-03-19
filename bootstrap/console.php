935207e93cc58120225b1cab73eaec75a0de0914
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-03-19 07:03:58 +0100

    register application bootstrap generators

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -2,7 +2,12 @@
 namespace KiwiSuite\Admin;
 
 /** @var ConsoleConfigurator $console */
+use KiwiSuite\Application\Console\BootstrapGenerateCommand;
+use KiwiSuite\Application\Console\BootstrapListCommand;
 use KiwiSuite\ApplicationConsole\ConsoleConfigurator;
+
+$console->addCommand(BootstrapListCommand::class);
+$console->addCommand(BootstrapGenerateCommand::class);
 
 $console->addDirectory(getcwd() . '/src/App/Console', true);
 $console->addDirectory(getcwd() . '/src/Admin/Console', true);
