1ed31358b01f3874863a71b7978e3587a2666980
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2018-08-06 17:01:40 +0200

    added config commands

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -4,6 +4,8 @@ namespace KiwiSuite\Admin;
 /** @var ConsoleConfigurator $console */
 use KiwiSuite\Application\Console\BootstrapGenerateCommand;
 use KiwiSuite\Application\Console\BootstrapListCommand;
+use KiwiSuite\Application\Console\ConfigGenerateCommand;
+use KiwiSuite\Application\Console\ConfigListCommand;
 use KiwiSuite\Application\Console\PublishCommand;
 use KiwiSuite\Application\Console\PublishListCommand;
 use KiwiSuite\ApplicationConsole\ConsoleConfigurator;
@@ -12,6 +14,8 @@ $console->addCommand(BootstrapListCommand::class);
 $console->addCommand(BootstrapGenerateCommand::class);
 $console->addCommand(PublishCommand::class);
 $console->addCommand(PublishListCommand::class);
+$console->addCommand(ConfigListCommand::class);
+$console->addCommand(ConfigGenerateCommand::class);
 
 $console->addDirectory(getcwd() . '/src/App/Console', true);
 $console->addDirectory(getcwd() . '/src/Admin/Console', true);
