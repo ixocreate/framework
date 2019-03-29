cf031fe2b8581ebcc8fc97616285c9a6cffb3a4b
Bernhard Efler <1978420+bernhard-efler@users.noreply.github.com>
2019-03-29 11:07:50 +0100

    add src folder to translation extraction

--- bootstrap/translator.php
+++ bootstrap/translator.php
@@ -6,5 +6,7 @@ namespace Ixocreate\Framework;
 /** @var \Ixocreate\Translation\Config\Configurator $translator */
 
 $translator->addExtractDirectory(getcwd() . '/templates');
+$translator->addExtractDirectory(getcwd() . '/src');
+
 $translator->setExtractTarget(getcwd() . '/resources/translation/extracted.json');
 $translator->setDefaultCatalogue("web");
