cdad7a8f887c804e7b4738fdb3f572c932b53164
Thomas Payer <1170097+thomaspayer@users.noreply.github.com>
2019-03-29 09:46:59 +0100

    prepare database classes

---
+++ src/Console/DatabasePrepareConsole.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
+declare(strict_types=1);
+
+namespace Ixocreate\Framework\Console;
+
+use Doctrine\Common\Cache\PhpFileCache;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Proxy\ProxyFactory;
+use Ixocreate\Application\ApplicationConfig;
+use Ixocreate\Contract\Command\CommandInterface;
+use Ixocreate\Database\Connection\Factory\ConnectionSubManager;
+use Ixocreate\Database\ORM\Mapping\EntityMapper;
+use Ixocreate\Database\Repository\EntityRepositoryMapping;
+use Ixocreate\Database\Repository\Factory\DoctrineRepositoryFactory;
+use Ixocreate\Database\Repository\Factory\RepositorySubManager;
+use Ixocreate\Database\Type\Generator\FileWriter;
+use Ixocreate\Database\Type\TypeConfig;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DatabasePrepareConsole extends Command implements CommandInterface
+{
+    /**
+     * @var ApplicationConfig
+     */
+    private $applicationConfig;
+    /**
+     * @var TypeConfig
+     */
+    private $typeConfig;
+    /**
+     * @var ConnectionSubManager
+     */
+    private $connectionSubManager;
+    /**
+     * @var RepositorySubManager
+     */
+    private $repositorySubManager;
+    /**
+     * @var EntityRepositoryMapping
+     */
+    private $entityRepositoryMapping;
+
+    public function __construct(
+        ApplicationConfig $applicationConfig,
+        TypeConfig $typeConfig,
+        ConnectionSubManager $connectionSubManager,
+        RepositorySubManager $repositorySubManager,
+        EntityRepositoryMapping $entityRepositoryMapping
+    ) {
+        parent::__construct(self::getCommandName());
+        $this->applicationConfig = $applicationConfig;
+        $this->typeConfig = $typeConfig;
+        $this->connectionSubManager = $connectionSubManager;
+        $this->repositorySubManager = $repositorySubManager;
+        $this->entityRepositoryMapping = $entityRepositoryMapping;
+    }
+
+    public static function getCommandName()
+    {
+        return 'prepare:database';
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        (new FileWriter())->write(
+            rtrim($this->applicationConfig->getPersistCacheDirectory(), '/'),
+            $this->typeConfig
+        );
+
+        foreach ($this->connectionSubManager->getServices() as $service) {
+            $connection = $this->connectionSubManager->get($service);
+
+            $configuration = new Configuration();
+            $configuration->setMetadataDriverImpl(
+                new EntityMapper($this->repositorySubManager, $this->entityRepositoryMapping)
+            );
+
+            $configuration->setRepositoryFactory(
+                new DoctrineRepositoryFactory($this->repositorySubManager, $this->entityRepositoryMapping)
+            );
+
+            $configuration->setProxyDir(sys_get_temp_dir());
+            $configuration->setProxyNamespace('Ixocreate\DoctrineProxy');
+            $configuration->setMetadataCacheImpl(new PhpFileCache(
+                $this->applicationConfig->getPersistCacheDirectory() . 'database/doctrine_metadata'
+            ));
+            $configuration->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_NEVER);
+
+            $entityManager = EntityManager::create(
+                $connection,
+                $configuration
+            );
+
+            $metadatas = $entityManager->getMetadataFactory()->getAllMetadata();
+
+            foreach ($metadatas as $metadata) {
+
+            }
+        }
+    }
+}
